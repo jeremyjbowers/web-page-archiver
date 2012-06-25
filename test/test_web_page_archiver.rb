77871211b87e197e0d2a0494989d54946fba31d9
Maarten Brouwers <github.com@murb.nl>
2012-06-25 16:01:48 +0200

    moved remote test to more predictable statically served github hosted page

--- test/test_web_page_archiver.rb
+++ test/test_web_page_archiver.rb
@@ -2,14 +2,15 @@ require 'helper'
 
 class TestWebPageArchiver < Test::Unit::TestCase
   def test_generate_remote
-    mhtml = WebPageArchiver::MhtmlGenerator.generate("https://rubygems.org/")
-    assert mhtml != ""
+    mhtml = WebPageArchiver::MhtmlGenerator.generate("http://murb.github.com/web-page-archiver/static/")
+    assert(mhtml.match('touXAzfbxedaI8CBEZgixpEx0oFD'))
+    assert(mhtml.match(/Content-Disposition: inline; filename=test.js\nContent-Type: application\/(.*)javascript\nContent-Location: (.*)test.js\nContent-Transfer-Encoding: Base64\nContent-Id: (.*)\n\nZnVuY3Rpb24gdGVzdCgpIHsKCWFsZXJ0KCd0ZXN0Jyk7Cn0=/))
   end
   
   def test_generate_local
     mhtml = WebPageArchiver::MhtmlGenerator.generate("fixtures/index.html")
     assert(mhtml.match('touXAzfbxedaI8CBEZgixpEx0oFD'))
-    assert(mhtml.match(/Content-Disposition: inline; filename=test.js\nContent-Type: application\/javascript\nContent-Location: (.*)test.js\nContent-Transfer-Encoding: Base64\nContent-Id: (.*)\n\nZnVuY3Rpb24gdGVzdCgpIHsKCWFsZXJ0KCd0ZXN0Jyk7Cn0=/))
+    assert(mhtml.match(/Content-Disposition: inline; filename=test.js\nContent-Type: application\/(.*)javascript\nContent-Location: (.*)test.js\nContent-Transfer-Encoding: Base64\nContent-Id: (.*)\n\nZnVuY3Rpb24gdGVzdCgpIHsKCWFsZXJ0KCd0ZXN0Jyk7Cn0=/))
   end
   
   
