e22de0c8b38c4b6dd42e9900569506b969621503
jeremyjbowers <jeremyjbowers@gmail.com>
2014-07-21 12:20:02 -0400

    Whoops, wrong file.

--- lib/web_page_archiver.rb
+++ lib/web_page_archiver.rb
@@ -124,14 +124,12 @@ module WebPageArchiver
         @src.puts ""
         #imgs
         @parser.search('img').each{|i|
-              uri = i.attr('src');
-              uri = join_uri( filename_or_uri, uri).to_s
-              uid = Digest::MD5.hexdigest(uri)
-              puts i.attr('src').to_s
-              if not i.attr('src').to_s.include?("pixel.quantserve.com")
-                @contents[uid] = {:uri=>uri}
-              end
-              i.set_attribute('src',"cid:#{uid}")
+            uri = i.attr('src');
+            uri = join_uri( filename_or_uri, uri).to_s
+            uid = Digest::MD5.hexdigest(uri)
+            puts i.attr('src').to_s
+            @contents[uid] = {:uri=>uri}
+            i.set_attribute('src',"cid:#{uid}")
           }
         #styles
         @parser.search('link[rel=stylesheet]').each{|i|
@@ -226,8 +224,10 @@ module WebPageArchiver
             uri = i.attr('src');
             uri = join_uri( filename_or_uri, uri).to_s
             uid = Digest::MD5.hexdigest(uri)
-            @contents[uid] = {:uri=>uri, :parser_ref=>i, :attribute_name=>'src'}
-            i.set_attribute('src',"cid:#{uid}")
+            if not i.attr('src').to_s.include?("pixel.quantserve.com")
+              @contents[uid] = {:uri=>uri, :parser_ref=>i, :attribute_name=>'src'}
+              i.set_attribute('src',"cid:#{uid}")
+            end
           }
         #styles
         @parser.search('link[rel=stylesheet]').each{|i|
