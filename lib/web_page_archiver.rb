0133371588732ef3619a0baafc0bce106f8bd8ab
jeremyjbowers <jeremyjbowers@gmail.com>
2014-07-21 15:31:43 -0400

    Cleanup.

--- lib/web_page_archiver.rb
+++ lib/web_page_archiver.rb
@@ -151,7 +151,13 @@ module WebPageArchiver
       @contents.each{ |k,v| @queue.push k }
       self.start_download_thread
       @threads.each{ |t| t.join }
-      @contents.each { |k,v| v[:parser_ref].set_attribute(v[:attribute_name],"data:#{v[:content_type]};base64,#{Base64.encode64(v[:body]).gsub(/\n/,'')}") }
+      @contents.each { |k,v|
+        content_benc = Base64.encode64(v[:body]).gsub(/\n/,'')
+        tag = v[:parser_ref]
+        attribute = v[:attribute_name]
+        content_type = v[:content_type]
+        tag.set_attribute(attribute,"data:#{content_type};base64,#{content_benc}")
+      }
     end
 
   end
